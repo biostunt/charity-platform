d5dd5bc449f40af98acf4dc98732ca8b605bbeb8
Кузнецов Никита Геннадьевич <nikita.kuznecov@gost-group.com>
2023-05-08 22:12:27 +0300

    Fix Donation domain

--- backend/src/modules/donation/domain/donation.domain.ts
+++ backend/src/modules/donation/domain/donation.domain.ts
@@ -1,9 +1,11 @@
 import { BaseDomain } from 'src/common/base/domain.base';
 import { Account } from '@modules/account/domain/account.domain';
 import { DonationStatusEnum } from './donation-status.enum';
+import { Goal } from '@modules/goal/domain/goal.domain';
 
 export interface Donation extends BaseDomain {
   status: DonationStatusEnum;
   owner: Account;
   value: number;
+  goal: Goal;
 }
