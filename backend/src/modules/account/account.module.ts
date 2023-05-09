bf7b150c0964ff64158d51c0f34dc702dc45fadc
Кузнецов Никита Геннадьевич <nikita.kuznecov@gost-group.com>
2023-05-09 20:09:17 +0300

    Attach login service to module

--- backend/src/modules/account/account.module.ts
+++ backend/src/modules/account/account.module.ts
@@ -8,15 +8,18 @@ import { GetAccountByIdService } from './features/get-account-by-id/get-account-
 import { CompanyAccountRepository } from './infrastructure/database/company-account.repository';
 import { UserAccountRepository } from './infrastructure/database/user-account.repository';
 import { RegisterAccountService } from '@modules/account/features/register-account/register-account.service';
+import { LoginAccountService } from '@modules/account/features/login-account/login-account.service';
+import { AppAuthModule } from '@core/app-auth/app-auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AccountEntity, UserAccountEntity, CompanyAccountEntity])],
+  imports: [TypeOrmModule.forFeature([AccountEntity, UserAccountEntity, CompanyAccountEntity]), AppAuthModule],
   providers: [
     AccountRepository,
     CompanyAccountRepository,
     UserAccountRepository,
     GetAccountByIdService,
     RegisterAccountService,
+    LoginAccountService,
   ],
   exports: [GetAccountByIdService],
 })
